e5108bc63597d3eb4d1b3011e15cb2569b7bfe31
Mahum <mahumxsajid@gmail.com>
2023-11-11 16:30:31 -0500

    Add function playRound to allow for one round of rock, paper, scissors game which includes playing the game using the standard rules

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
+//numbers respresenting the options to be randomly selected
 const ROCK = 0, PAPER = 1, SCISSORS = 2;
 
+//function to get computer's selection
 function getComputerChoice () {
 
     //randomly select a number that represents rock, paper, or scissors
@@ -17,4 +19,43 @@ function getComputerChoice () {
     }
 
 }
+
+//function to play one round of game
+function playRound (playerSelection, computerSelection) {
+
+    //to allow for case-insensitive input
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            return "It's a tie!";
+        } else if (computerSelection === "paper") {
+            return "You lose! Paper beats Rock";
+        } else {
+            return "You win! Rocks beats Scissors";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper beats Rock";
+        } else if (computerSelection === "paper") {
+            return "It's a tie!";
+        } else {
+            return "You lose! Scissors beat Paper";
+        }
+    } else {
+        if (computerSelection === "rock") {
+            return "You lose! Rock beats Scissors";
+        } else if (computerSelection === "paper") {
+            return "You win! Scissors beats Paper";
+        } else {
+            return "It's a tie!";
+        }
+    }
+}
+
+let playerSelection = "rocK";
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
 
